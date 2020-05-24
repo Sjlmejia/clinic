b6a99c65b483a904065cbcbaf39a10ad8ef1faf8
Sjlmejia <jorgemejia2304@gmail.com>
2020-05-23 20:43:43 -0500

    update permissons

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const app = express();
 mongoose
   .connect(
     "mongodb://jorge:" +
-      "jorgemejia3223L" +
+      process.env.MONGO_ATLAS_PW +
       "@cluster0-shard-00-00-nhims.mongodb.net:27017,cluster0-shard-00-01-nhims.mongodb.net:27017,cluster0-shard-00-02-nhims.mongodb.net:27017/medicamentosDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
       , {useNewUrlParser: true})
   .then(() => {
